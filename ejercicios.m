826cf16b56e387ce747474cf142cf5e706e50324
Ítalo Garleni Rodríguez <italogar6@hotmail.com>
2017-03-04 18:22:38 +0100

    Moravec algorithm
    this algorithm is used to find figures onto an image

--- ejercicios.m
+++ ejercicios.m
@@ -82,6 +82,50 @@ figure,imshow(Iest);
 %4.- Analyze distorsion1.jpg and apply different tecniques to improve it
 %(delete noise). Specifically, Gaussian smoothing and 'motion' filter. Can
 %you find another way to improve its quality?
+J=imread('distorsion2.jpg');
+H = fspecial('motion',20,45);
+MotionBlur = imfilter(J,H,'replicate');        
+fg=fspecial('gaussian',10,1.5);
+R_gauss=imfilter(J,fg);
+figure, subplot (1,3,1),imshow(J),title('original'),...
+    subplot (1,3,2), imshow(R_gauss),title('Gaussian'),...
+    subplot (1,3,3), imshow(MotionBlur),title('Motion Blurred Image')
 
+%5.- Get corners of formas.png image using MORAVEC method (on pseudo_moravec.m)
+I = imread('formas.png');
+Isize = size(I);
+h = conv2([1 -1], I);
+v = conv2([1 -1]', I);
+diag1 = conv2([1 0 ; 0  1], I);
+diag2 = conv2([0 1; -1 0], I);
 
-%5.- Get corners of formas.png image using moravec method (on pseudo_moravec.m)
+%get the min
+for i = 2:(Isize(1)-1)
+    for j = 2:(Isize(2)-1)
+        c(i,j) = min (min( sum(sum(abs(h(i-1:i+1,j-1:j+1)))),...
+            sum(sum(abs(v(i-1:i+1,j-1:j+1))))),...
+            min( sum(sum(abs(diag1(i-1:i+1,j-1:j+1)))),...
+            sum(sum(abs(diag1(i-1:i+1,j-1:j+1))))));
+    end
+end
+
+%normalize
+maxC = max(max(c));
+c = c / maxC;
+
+%find corners
+corners = c;
+Csize = size(c);
+for i = 2:(Csize(1)-1)
+    for j = 2:(Csize(2)-1)
+        square = c((i-1):(i+1),(j-1):(j+1));
+        if (max(max(square)) == c(i,j))
+            corners(i,j) = 255;
+        else
+            corners (i,j) = 0;
+        end
+    end
+end
+
+imshow(corners)
+
